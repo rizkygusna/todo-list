6c68a6fefd010241ab469509ee90556bbccb3d74
rizkygusna <rizki.farid1998@gmail.com>
2021-03-29 22:32:06 +0700

    adding list command functionality

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ while (userInput.toLowerCase()!=='quit'){
         console.log(todoList);
         alert("To do has been added")
     }
+    else if(userInput==='list'){
+        if(todoList.length<1){
+            alert("To do is empty")
+        }
+        else{
+            console.log('**************************');
+            for(let i = 0; i<todoList.length; i++){
+                console.log(`${i}: ${todoList[i]}`);
+            }
+            console.log('**************************')
+        }
+    }
 }
 
 if (userInput.toLowerCase()==='quit'){
